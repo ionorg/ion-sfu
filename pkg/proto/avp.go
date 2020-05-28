tribe-health/ion

Togglable processors

pkg/proto/avp.go
@@ -1,0 +1,11 @@
+package proto
+
+import "encoding/json"
+
+// ProcessInfo describes an a/v process
+type ProcessInfo struct {
+	MID          string          `json:"mid"`
+	RID          string          `json:"rid"`
+	Processor    string          `json:"processor"`
+	ClientConfig json.RawMessage `json:"config"`
+}

ionorg/ion-sfu

Former-commit-id: 6037b85f0126babedd9bf0c0311b6aa566310cdb

pkg/proto/avp.go
@@ -1,0 +1,11 @@
+package proto
+
+import "encoding/json"
+
+// ElementInfo describes an a/v process
+type ElementInfo struct {
+	MID    string          `json:"mid"`
+	RID    string          `json:"rid"`
+	Type   string          `json:"type"`
+	Config json.RawMessage `json:"config"`
+}