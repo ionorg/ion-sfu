b9d3861c0213cff24a2bbd0fbdea9760eefc4aca
Tarrence van As <tarrence13@gmail.com>
2020-06-29 10:37:22 -0400

    update examples
    Former-commit-id: feb23772ceaff28c5e15599d8596b0d81c405aaf

--- examples/sub-to-browser/main.go
+++ examples/sub-to-browser/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -32,12 +33,36 @@ func main() {
 	mid := os.Args[1]
 
 	ctx := context.Background()
-	sub, err := c.Subscribe(ctx, &sfu.SubscribeRequest{Mid: mid, Description: &subOffer})
+	stream, err := c.Subscribe(ctx)
 
 	if err != nil {
 		log.Panicf("Error subscribing: %v", err)
 	}
 
-	fmt.Printf("\n\nsub mid: %s", sub.Mid)
-	fmt.Printf("\nsub answer: %s\n", signal.Encode(sub.Description))
+	stream.Send(&sfu.SubscribeRequest{Mid: mid, Payload: &sfu.SubscribeRequest_Connect{
+		Connect: &sfu.Connect{
+			Description: &subOffer,
+		},
+	}})
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			// WebRTC Transport closed
+			fmt.Println("WebRTC Transport Closed")
+			return
+		}
+
+		if err != nil {
+			log.Fatalf("Error receving publish response: %v", err)
+		}
+
+		switch payload := res.Payload.(type) {
+		case *sfu.SubscribeReply_Connect:
+			// Output the mid and answer in base64 so we can paste it in browser
+			fmt.Printf("\nsub mid: %s", res.Mid)
+			fmt.Printf("\nsub answer: %s", signal.Encode(payload.Connect.Description))
+			return
+		}
+	}
 }
