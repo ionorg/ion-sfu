84a8f1a62588878f56f18edaba095e511cba4cf4
tarrencev <tarrence13@gmail.com>
2020-09-02 09:44:05 -0400

    update logging/improve some code

--- pkg/errors.go
+++ pkg/errors.go
@@ -4,7 +4,6 @@ import "errors"
 
 var (
 	errPeerConnectionInitFailed = errors.New("pc init failed")
-	errChanClosed               = errors.New("channel closed")
 	errPtNotSupported           = errors.New("payload type not supported")
 	errMethodNotSupported       = errors.New("method not supported")
 	errReceiverClosed           = errors.New("receiver closed")
