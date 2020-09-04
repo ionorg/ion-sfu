8af83f70344a1c8b5fca5aa07de54a3bc30be96f
tarrencev <tarrence13@gmail.com>
2020-09-04 18:49:19 -0400

    ci(workflow): build grpc docker

--- .github/install-hooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cp "$SCRIPT_PATH/hooks/pre-commit.sh" "$SCRIPT_PATH/../.git/hooks/pre-commit"
-cp "$SCRIPT_PATH/hooks/pre-push.sh" "$SCRIPT_PATH/../.git/hooks/pre-push"
