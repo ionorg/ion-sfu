8b90e84cfe3262ae6dd930262c14c9c11568dbbb
CloudWebRTC <duanweiwei1982@gmail.com>
2020-12-03 22:45:01 +0800

    Add support for grpc-web. (#244)
    * Add support for grpc-web.
    
    * cleanup.
    
    * Support grpc/grpc-web using same port.

---
+++ cmd/signal/grpc/grpc-web/main.go
@@ -0,0 +1,113 @@
+// Package cmd contains an entrypoint for running an ion-sfu instance.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	log "github.com/pion/ion-log"
+	sfu "github.com/pion/ion-sfu/pkg"
+	"github.com/spf13/viper"
+
+	"github.com/pion/ion-sfu/cmd/signal/grpc/server"
+)
+
+type grpcConfig struct {
+	Port string `mapstructure:"port"`
+}
+
+// Config defines parameters for configuring the sfu instance
+type Config struct {
+	sfu.Config `mapstructure:",squash"`
+	GRPC       grpcConfig `mapstructure:"grpc"`
+}
+
+var (
+	conf = Config{}
+	file string
+	addr string
+)
+
+const (
+	portRangeLimit = 100
+)
+
+func showHelp() {
+	fmt.Printf("Usage:%s {params}\n", os.Args[0])
+	fmt.Println("      -c {config file}")
+	fmt.Println("      -a {listen addr}")
+	fmt.Println("      -h (show help info)")
+}
+
+func load() bool {
+	_, err := os.Stat(file)
+	if err != nil {
+		return false
+	}
+
+	viper.SetConfigFile(file)
+	viper.SetConfigType("toml")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("config file %s read failed. %v\n", file, err)
+		return false
+	}
+	err = viper.GetViper().Unmarshal(&conf)
+	if err != nil {
+		fmt.Printf("sfu config file %s loaded failed. %v\n", file, err)
+		return false
+	}
+
+	if len(conf.WebRTC.ICEPortRange) > 2 {
+		fmt.Printf("config file %s loaded failed. range port must be [min,max]\n", file)
+		return false
+	}
+
+	if len(conf.WebRTC.ICEPortRange) != 0 && conf.WebRTC.ICEPortRange[1]-conf.WebRTC.ICEPortRange[0] < portRangeLimit {
+		fmt.Printf("config file %s loaded failed. range port must be [min, max] and max - min >= %d\n", file, portRangeLimit)
+		return false
+	}
+
+	fmt.Printf("config %s load ok!\n", file)
+	return true
+}
+
+func parse() bool {
+	flag.StringVar(&file, "c", "config.toml", "config file")
+	flag.StringVar(&addr, "a", ":9090", "address to use")
+	help := flag.Bool("h", false, "help info")
+	flag.Parse()
+	if !load() {
+		return false
+	}
+
+	if *help {
+		return false
+	}
+	return true
+}
+
+func main() {
+	if !parse() {
+		showHelp()
+		os.Exit(-1)
+	}
+
+	fixByFile := []string{"asm_amd64.s", "proc.go", "icegatherer.go"}
+	fixByFunc := []string{}
+	log.Init(conf.Log.Level, fixByFile, fixByFunc)
+
+	log.Infof("--- Starting SFU Node ---")
+	options := server.DefaultWrapperedServerOptions()
+	options.EnableTLS = false
+	options.Addr = addr
+	options.AllowAllOrigins = true
+	options.UseWebSocket = true
+	s := server.NewWrapperedGRPCWebServer(options, sfu.NewSFU(conf.Config))
+	if err := s.Serve(); err != nil {
+		log.Panicf("failed to serve: %v", err)
+	}
+	select {}
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.13
 
 require (
 	github.com/bep/debounce v1.2.0
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/gammazero/deque v0.0.0-20201010052221-3932da5530cc
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/gorilla/websocket v1.4.2
+	github.com/improbable-eng/grpc-web v0.13.0
 	github.com/lucas-clemente/quic-go v0.19.1 // indirect
 	github.com/lucsky/cuid v1.0.2
 	github.com/pion/ice/v2 v2.0.10 // indirect
@@ -15,11 +17,14 @@ require (
 	github.com/pion/rtp v1.6.1
 	github.com/pion/sdp/v3 v3.0.3
 	github.com/pion/webrtc/v3 v3.0.0-beta.12.0.20201115002753-64bbf7eea97d
+	github.com/rs/cors v1.7.0 // indirect
+	github.com/sirupsen/logrus v1.2.0
+	github.com/soheilhy/cmux v0.1.4
 	github.com/sourcegraph/jsonrpc2 v0.0.0-20200429184054-15c2290dcb37
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/crypto v0.0.0-20201112155050-0c6587e931a9 // indirect
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
