4da7bf247d5e1ad0b0686ca62bc2791043773637
Nikita Konev <nikit.cpp@yandex.ru>
2021-02-26 12:43:16 +0300

    make turn ports configurable
    make turn.portrange config value
    
    add turn range check
    
    improve config.toml comment

--- pkg/sfu/sfu.go
+++ pkg/sfu/sfu.go
@@ -76,7 +76,7 @@ func NewWebRTCTransportConfig(c Config) WebRTCTransportConfig {
 
 	var icePortStart, icePortEnd uint16
 
-	if c.Turn.Enabled {
+	if c.Turn.Enabled && len(c.Turn.PortRange) == 0 {
 		icePortStart = sfuMinPort
 		icePortEnd = sfuMaxPort
 	} else if len(c.WebRTC.ICEPortRange) == 2 {
